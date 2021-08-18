8ccf6e376034e53ef20f39ec8e847ebe2f1d8935
777AMR <meyram.ramazanov@mail.ru>
2021-08-18 21:14:03 +0600

    9_ XSS - Cross Site Scripting

--- ee/src/servlets/TempServlet.java
+++ ee/src/servlets/TempServlet.java
@@ -6,52 +6,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
-import java.util.Map;
 
 @WebServlet("/temp")
 public class TempServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String one = req.getParameter("one");
-//        System.out.println(one);
-
-
-//        String[] ones = req.getParameterValues("one");
-//        for (String s : ones) {
-//            System.out.println(s);
-//        }
-
-//        Enumeration<String> parameterNames = req.getParameterNames();
-//        while (parameterNames.hasMoreElements()) {
-//            String elementName = parameterNames.nextElement();
-//            System.out.println(elementName + " = " + req.getParameter(elementName));
-//        }
-
-//        Map<String, String[]> parameterMap = req.getParameterMap();
-//        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-//            System.out.println(entry.getKey() + " = " + entry.getValue());
-//        }
-
-//        System.out.println(req.getRequestURL());
-//        System.out.println(req.getServletPath());
-//        System.out.println(req.getRemoteHost());
-//        System.out.println(req.getRequestURI());
-//        System.out.println(req.getLocalPort());
-//        System.out.println(req.getQueryString());
-
         String one = req.getParameter("one");
-        String two = req.getParameter("two");
+        one = one == null ? "" : one.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
         resp.getWriter().write(
                 "<html>" +
                         "<head></head>" +
                         "<body>" +
                         "one = " + one +
-                        ", two = " + two +
-                        "<form action='temp' method='get'>" +
-                        "<input type='text' name = 'one'/>" +
-                        "<input type='text' name = 'two'/>" +
+                        "<form action='temp' method='post'>" +
+                        "<textarea name = 'one'></textarea>" +
                         "<input type='submit' name = 'submit'/>" +
                         "</form>" +
                         "</body>" +
@@ -60,20 +29,6 @@ public class TempServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String one = req.getParameter("one");
-        String two = req.getParameter("two");
-        resp.getWriter().write(
-                "<html>" +
-                        "<head></head>" +
-                        "<body>" +
-                        "one = " + one +
-                        ", two = " + two +
-                        "<form action='temp' method='post'>" +
-                        "<input type='text' name = 'one'/>" +
-                        "<input type='text' name = 'two'/>" +
-                        "<input type='submit' name = 'submit'/>" +
-                        "</form>" +
-                        "</body>" +
-                        "</html>");
+        doGet(req, resp);
     }
 }
