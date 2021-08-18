beb89d691c103b4bb2483900eb919ddd83284f02
777AMR <meyram.ramazanov@mail.ru>
2021-08-18 17:06:16 +0600

    6_ Жизненый цикл сервлета

--- ee/src/servlets/ServletLiveCycle.java
+++ ee/src/servlets/ServletLiveCycle.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,8 +11,16 @@ import java.io.IOException;
 
 @WebServlet("/LiveServlet")
 public class ServletLiveCycle extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("initialization");
+        super.init();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("loading");
         String s = "<html>" +
                 "<header><title>This is title</title></header>" +
                 "<body>" +
@@ -18,5 +28,23 @@ public class ServletLiveCycle extends HttpServlet {
                 "</body>" +
                 "</html>";
         resp.getWriter().write(s);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("loading");
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("service");
+        super.service(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+        super.destroy();
     }
 }
