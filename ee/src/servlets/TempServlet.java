6f8575de02fc5c2e61bcddcaa886bf366f357c3c
777AMR <meyram.ramazanov@mail.ru>
2021-08-18 20:53:27 +0600

    8_ URL параметры

--- ee/src/servlets/TempServlet.java
+++ ee/src/servlets/TempServlet.java
@@ -6,38 +6,74 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.util.Enumeration;
+import java.util.Map;
 
 @WebServlet("/temp")
 public class TempServlet extends HttpServlet {
 
-    static int i = 0;
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        synchronized (this) {
-            for (int j = 0; j < 1_000_000; j++) {
-                i++;
-            }
-            System.out.println(i);
-            System.out.println(Thread.currentThread().getName());
-        }
+//        String one = req.getParameter("one");
+//        System.out.println(one);
+
+
+//        String[] ones = req.getParameterValues("one");
+//        for (String s : ones) {
+//            System.out.println(s);
+//        }
+
+//        Enumeration<String> parameterNames = req.getParameterNames();
+//        while (parameterNames.hasMoreElements()) {
+//            String elementName = parameterNames.nextElement();
+//            System.out.println(elementName + " = " + req.getParameter(elementName));
+//        }
+
+//        Map<String, String[]> parameterMap = req.getParameterMap();
+//        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+//            System.out.println(entry.getKey() + " = " + entry.getValue());
+//        }
+
+//        System.out.println(req.getRequestURL());
+//        System.out.println(req.getServletPath());
+//        System.out.println(req.getRemoteHost());
+//        System.out.println(req.getRequestURI());
+//        System.out.println(req.getLocalPort());
+//        System.out.println(req.getQueryString());
+
+        String one = req.getParameter("one");
+        String two = req.getParameter("two");
+        resp.getWriter().write(
+                "<html>" +
+                        "<head></head>" +
+                        "<body>" +
+                        "one = " + one +
+                        ", two = " + two +
+                        "<form action='temp' method='get'>" +
+                        "<input type='text' name = 'one'/>" +
+                        "<input type='text' name = 'two'/>" +
+                        "<input type='submit' name = 'submit'/>" +
+                        "</form>" +
+                        "</body>" +
+                        "</html>");
     }
 
-    public static void main(String[] args) {
-        for (int i = 0; i < 2; i++) {
-            new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        URLConnection connection = new URL("http://localhost:8080/temp").openConnection();
-                        connection.getInputStream();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }.start();
-        }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String one = req.getParameter("one");
+        String two = req.getParameter("two");
+        resp.getWriter().write(
+                "<html>" +
+                        "<head></head>" +
+                        "<body>" +
+                        "one = " + one +
+                        ", two = " + two +
+                        "<form action='temp' method='get'>" +
+                        "<input type='text' name = 'one'/>" +
+                        "<input type='text' name = 'two'/>" +
+                        "<input type='submit' name = 'submit'/>" +
+                        "</form>" +
+                        "</body>" +
+                        "</html>");
     }
 }
