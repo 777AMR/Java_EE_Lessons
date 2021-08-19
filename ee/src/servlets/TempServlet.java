3c96b3b6bbb9b61615c18eb2b6da62e1ceddf1a1
777AMR <meyram.ramazanov@mail.ru>
2021-08-19 11:45:48 +0600

    12_ Return status

--- ee/src/servlets/TempServlet.java
+++ ee/src/servlets/TempServlet.java
@@ -6,17 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.zip.GZIPOutputStream;
 
 @WebServlet("/temp")
 public class TempServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if (req.getHeader("Accept-Encoding").contains("gzip")) {
-            PrintWriter printWriter = new PrintWriter(new GZIPOutputStream(resp.getOutputStream()));
-            printWriter.write("hello world");
-        }
+//        resp.setStatus(HttpServletResponse.SC_OK);
+//        resp.sendRedirect("/hello");
+//        resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad");
+
+//        resp.setHeader("Refresh", "1");
+//        System.out.println("hello");
+
+        resp.setHeader("Refresh", "5;URL=https://google.com");
     }
 }
