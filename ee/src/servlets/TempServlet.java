221c2e93a8285f3589e15d3ff6855cb435655d25
777AMR <meyram.ramazanov@mail.ru>
2021-08-19 11:05:25 +0600

    10_ Headers

--- ee/src/servlets/TempServlet.java
+++ ee/src/servlets/TempServlet.java
@@ -6,29 +6,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet("/temp")
 public class TempServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String one = req.getParameter("one");
-        one = one == null ? "" : one.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-        resp.getWriter().write(
-                "<html>" +
-                        "<head></head>" +
-                        "<body>" +
-                        "one = " + one +
-                        "<form action='temp' method='post'>" +
-                        "<textarea name = 'one'></textarea>" +
-                        "<input type='submit' name = 'submit'/>" +
-                        "</form>" +
-                        "</body>" +
-                        "</html>");
-    }
+//        Enumeration<String> headerNames = req.getHeaderNames();
+//        while (headerNames.hasMoreElements()){
+//            String s = headerNames.nextElement();
+//            System.out.println(s + " = " + req.getHeader(s));
+//        }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+        System.out.println(req.getAuthType());
+        System.out.println(req.getContentLength());
+        System.out.println(req.getContentType());
+        System.out.println(req.getMethod());
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getQueryString());
+        System.out.println(req.getProtocol());
+
+        resp.setHeader("Content-Length:", "100");
     }
 }
