4df8ec4957f317a4df9860d976912133a93090d7
777AMR <meyram.ramazanov@mail.ru>
2021-08-23 14:04:06 +0600

    37_ WebXml 10_ Фильтры

---
+++ ee/src/jsp/MyNewFilter.java
@@ -0,0 +1,40 @@
+package jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyNewFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println(filterConfig.getInitParameter("name"));
+        System.out.println("init filter");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.getWriter().write("before ");
+        if (true) {
+            filterChain.doFilter(servletRequest, new MyResponseWrapper((HttpServletResponse)servletResponse));
+        }
+        servletResponse.getWriter().write(" after");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy filter");
+    }
+}
+
+class MyResponseWrapper extends HttpServletResponseWrapper {
+    public MyResponseWrapper(HttpServletResponse response) {
+        super(response);
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+
+        return super.getWriter();
+    }
+}
