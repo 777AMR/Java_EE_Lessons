464d50270d112616dfdd4ffc80ecd303d51720fb
777AMR <meyram.ramazanov@mail.ru>
2021-08-27 19:34:10 +0600

    46_ CDI 3_ Quantifier

--- ee/src/ee/cdi/DependencyInjectionExample.java
+++ ee/src/ee/cdi/DependencyInjectionExample.java
@@ -1,42 +1,68 @@
 package ee.cdi;
 
 import javax.inject.Inject;
+import javax.inject.Qualifier;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @WebServlet("/diExample")
 public class DependencyInjectionExample extends HttpServlet {
-//    @Inject
-    Student student;
+    @Inject
+    @StudentAnnotation
+    Person person;
 
     @Inject
-    public DependencyInjectionExample(Student student) {
-        this.student = student;
-    }
-
-//    @Inject
-//    public void setStudent(Student student) {
-//        this.student = student;
-//    }
+    @WorkerAnnotation
+    Person worker;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(student.getName());
+        System.out.println(person.getName());
+        System.out.println(worker.getName());
     }
 }
 
-class Student{
+@Qualifier
+@Retention(RUNTIME)
+@Target({FIELD, TYPE, METHOD})
+@interface StudentAnnotation{}
+
+interface Person{
+    String getName();
+}
+
+@StudentAnnotation
+class Student implements Person{
     private String name;
 
     public String getName() {
+        name = "Student";
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+}
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({FIELD, TYPE, METHOD})
+@interface WorkerAnnotation{}
+
+@WorkerAnnotation
+class Worker implements Person{
+    @Override
+    public String getName() {
+        return "worker";
     }
 }
