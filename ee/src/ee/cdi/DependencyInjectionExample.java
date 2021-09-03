2248900842fea7a25c5aa9eb519cc9d20a3570c0
777AMR <meyram.ramazanov@mail.ru>
2021-09-03 15:00:31 +0600

    50_ CDI 7_ Injection point

--- ee/src/ee/cdi/DependencyInjectionExample.java
+++ ee/src/ee/cdi/DependencyInjectionExample.java
@@ -1,7 +1,7 @@
 package ee.cdi;
 
-import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,36 +9,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet("/diExample")
 public class DependencyInjectionExample extends HttpServlet {
+//    Logger logger = Logger.getLogger(DependencyInjectionExample.class.getName());
     @Inject
-    Car car;
-
+    Logger logger;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write(car.name);
+        logger.log(Level.ALL, "message");
     }
 }
 
-class Producer {
+class MyLogger {
     @Produces
-    Car getCar() {
-        return new Car("Mitsubishi");
-    }
-    public void clean(@Disposes Car car){
-        car.clean();
-    }
-}
-
-class Car {
-    String name;
-
-    public Car(String name) {
-        this.name = name;
-    }
-
-    public void clean() {
-        System.out.println("car clean");
+    public Logger getLogger(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
     }
 }
